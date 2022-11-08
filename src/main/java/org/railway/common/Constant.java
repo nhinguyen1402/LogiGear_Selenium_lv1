58ec22682149d1a617b691872b354080ead5a740
Nhi Nguyen <nhi.thuc.nguyen@logigear.com>
2022-11-08 17:11:35 +0700

    Run TC01-Login successfully

---
+++ src/main/java/org/railway/common/Constant.java
@@ -0,0 +1,7 @@
+package org.railway.common;
+
+public class Constant {
+    public static final String RAILWAY_URL = "http://railwayb2.somee.com";
+    public static final String USERNAME = "nhinguyen143099@gmail.com";
+    public static final String PASSWORD = "nhi12345";
+}
