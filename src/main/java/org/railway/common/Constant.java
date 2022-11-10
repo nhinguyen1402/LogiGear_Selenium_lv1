8455fd9ba0809cad25fba5a301ec05bd0886446a
Nhi Nguyen <nhi.thuc.nguyen@logigear.com>
2022-11-10 14:00:25 +0700

    Update TC02

--- src/main/java/org/railway/common/Constant.java
+++ src/main/java/org/railway/common/Constant.java
@@ -1,7 +1,7 @@
 package org.railway.common;
 
 public class Constant {
-    public static final String RAILWAY_URL = "http://railwayb2.somee.com";
+    public static final String RAILWAY_HOME_URL = "http://www.railwayqa.somee.com/Page/HomePage.cshtml";
     public static final String USERNAME = "nhinguyen143099@gmail.com";
     public static final String PASSWORD = "nhi12345";
 }
